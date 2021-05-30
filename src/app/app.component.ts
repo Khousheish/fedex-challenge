1dda4178402aa9bcce3ee5fa2f6b209700ac73f1
Hassan Khousheish <hassan@backbase.com>
2021-05-30 15:22:12 +0200

    Configure App Module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { TranslateService } from '@ngx-translate/core';
+
+import { ThemeSwitcherService } from '@Components/theme-switcher/services/theme-switcher.service';
+import { SupportedLanguages } from '@Enums/supported-languages.enum';
+import { environment } from '@Environment';
 
 @Component({
   selector: 'fc-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-  title = 'fedex-challenge';
+export class AppComponent implements OnInit {
+
+  public constructor(
+    private readonly titleService: Title,
+    private readonly translateService: TranslateService,
+    private readonly themeSwitcherService: ThemeSwitcherService,
+  ) {
+  }
+
+  public ngOnInit(): void {
+    const supportedLangs: SupportedLanguages[] = [SupportedLanguages.English, SupportedLanguages.Dutch];
+    this.translateService.setDefaultLang(environment.defaultLang);
+    this.translateService.use(environment.defaultLang);
+    supportedLangs.forEach((language: SupportedLanguages): void => {
+      this.translateService.reloadLang(language);
+    });
+    this.titleService.setTitle(environment.appTitle);
+    this.themeSwitcherService.initializeTheme();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,83 @@
-import { NgModule } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { LOCALE_ID, NgModule, Provider } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { MessageService } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
+
+import { getInitialState, REDUCER_PROVIDER, reducerToken } from '@AppStore';
+import { LayoutModule } from '@Components/layout/layout.module';
+import { ThemeSwitcherModule } from '@Components/theme-switcher/theme-switcher.module';
+import { environment } from '@Environment';
+import { ERROR_HANDLING_INTERCEPTOR_PROVIDER } from '@Interceptors/error-handling.interceptor';
+import { MultiTranslateLoader } from '@Loaders/multi-translate.loader';
+import { LocalizationService } from '@Services/localization.service';
+import { ErrorFacade } from '@Store/error/error.facade';
+import { ComponentsType, ModulesType } from '@Types/module.types';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ErrorToastComponent } from './shared/components/error-toast/error-toast.component';
+
+const imports: ModulesType = [
+  HttpClientModule,
+  BrowserModule,
+  AppRoutingModule,
+  LayoutModule,
+  ThemeSwitcherModule,
+  BrowserAnimationsModule,
+  ToastModule,
+  EffectsModule.forRoot([]),
+  StoreModule.forRoot(reducerToken, { initialState: getInitialState }),
+  StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !environment.production }),
+  TranslateModule.forRoot({
+    loader: {
+      provide: TranslateLoader,
+      useClass: MultiTranslateLoader,
+      deps: [ HttpClient ],
+    },
+  }),
+];
+
+const declarations: ComponentsType = [
+  AppComponent,
+  ErrorToastComponent,
+];
+
+const providers: Provider[] = [
+  ErrorFacade,
+  REDUCER_PROVIDER,
+  ERROR_HANDLING_INTERCEPTOR_PROVIDER,
+  MessageService,
+  LocalizationService,
+  {
+    provide: LOCALE_ID,
+    deps: [
+      LocalizationService,
+    ],
+    useFactory: (localizationService: LocalizationService): string => (
+      localizationService.getLanguage()
+    ),
+  },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    ...imports,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    ...declarations,
+  ],
+  providers: [
+    ...providers,
+  ],
+  bootstrap: [
+    AppComponent,
+  ],
 })
-export class AppModule { }
+export class AppModule {
+}
